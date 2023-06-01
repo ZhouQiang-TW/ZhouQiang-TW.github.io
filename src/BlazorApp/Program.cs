6b7ac58799135346aed3aa693278d5f1ba60ad98
Qiang Zhou <qiang.zhou1@thoughtworks.com>
2023-06-01 08:06:26 +0800

    [zhouqiang] #n/a add markdown and footer

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -2,6 +2,7 @@ using BlazorApp;
 using BlazorApp.Pages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 
 builder.Services.AddScoped<IndexRazor>();
 builder.Services.AddScoped<PortfolioRazor>();
