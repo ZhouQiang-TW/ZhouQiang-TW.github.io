5df6699812fbfd70a0a3912382938d8ac25a0fa4
Qiang Zhou <qiang.zhou1@thoughtworks.com>
2023-06-01 08:42:00 +0800

    [zhouqiang] #n/a cleanup

--- src/BlazorApp/Shared/MainLayout.razor.js
+++ src/BlazorApp/Shared/MainLayout.razor.js
@@ -1,3 +1,6 @@
-﻿export function showPrompt(message) {
+﻿export const SayHelloFromJs =(message)=>{
+    console.log(message);
+};
+export function showPrompt(message) {
     return prompt(message, 'Type anything here');
 }
